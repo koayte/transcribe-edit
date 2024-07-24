32151ed888e0802327307600a640de7327a351f7
koayte <te.koay99@gmail.com>
2024-07-24 10:49:08 +0800

    Add transcription feature with front-end.

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from tool import pages # import blueprint called pages 
 
+
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(pages.bp)
--- tool/pages.py
+++ tool/pages.py
@@ -1,4 +1,12 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, Flask
+import os, requests, json
+from os.path import join, dirname
+
+# Env 
+from dotenv import load_dotenv
+
+# Whisper AI speech to text 
+import whisper 
 
 """
 Create an instance of Blueprint (class) named bp. 
@@ -6,10 +14,60 @@ Create an instance of Blueprint (class) named bp.
 """
 bp = Blueprint("pages", __name__)
 
-@bp.route("/")
-def home():
-    return render_template("pages/home.html")
 
-@bp.route("/about")
-def about():
-    return render_template("pages/about.html")
+
+######################## RECORD OR UPLOAD ########################
+@bp.route("/", methods = ["GET", "POST"])
+def home():
+    # dotenv_path = join(dirname(__file__), '.env')
+    # load_dotenv(dotenv_path)
+    # OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
+
+    # response = requests.post(
+    #     url="https://openrouter.ai/api/v1/chat/completions",
+    #     headers={
+    #         "Authorization": f"Bearer {OPENROUTER_API_KEY}",
+    #     },
+    #     data=json.dumps({
+    #         "model": "microsoft/phi-3-mini-128k-instruct:free", 
+    #         "messages": [
+    #         { "role": "user", "content": "What is the meaning of life?" }
+    #         ]
+    #     })
+    # )
+
+    # response_dict = json.loads(response.text)
+    # choices_dict = response_dict.get("choices")[0]
+    # message_dict = choices_dict.get("message")
+    # role = message_dict.get("assistant")
+    # content = message_dict.get("content")
+    # return render_template("pages/home.html", role=role, content=content)
+    return render_template("pages/home.html", warning="")
+
+def is_allowed_file(file):
+    return file.filename.endswith('.mp3')
+
+def transcribe(audio):
+    transcribe_model = whisper.load_model("base")
+    transcribe_result = transcribe_model.transcribe(audio)
+    transcription = transcribe_result["text"]
+    return transcription
+
+@bp.route("/upload", methods = ['POST'])
+def upload(): 
+    UPLOAD_FOLDER = '/user-uploads/'
+    if request.method == 'POST':
+        audio = request.files['audio-file']
+        if audio and is_allowed_file(audio):
+            filepath = "user-uploads/" + audio.filename
+            audio.save(filepath)
+            transcription = transcribe(filepath)
+            return render_template("pages/transcribe.html", transcription=transcription)
+        else:
+            return render_template("pages/home.html", warning="Please select an .mp3 file.")
+
+
+
+@bp.route("/past")
+def past():
+    return render_template("pages/past.html")
