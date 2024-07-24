7bd1db5b91dfbf14116e15693da2c0a86ef4fa83
koayte <te.koay99@gmail.com>
2024-07-24 16:18:13 +0800

    Add copy and edit features to transcription results.

--- tool/pages.py
+++ tool/pages.py
@@ -19,6 +19,7 @@ bp = Blueprint("pages", __name__)
 ######################## RECORD OR UPLOAD ########################
 @bp.route("/", methods = ["GET", "POST"])
 def home():
+    
     # dotenv_path = join(dirname(__file__), '.env')
     # load_dotenv(dotenv_path)
     # OPENROUTER_API_KEY = os.environ.get("OPENROUTER_API_KEY")
@@ -61,8 +62,8 @@ def upload():
         if audio and is_allowed_file(audio):
             filepath = "user-uploads/" + audio.filename
             audio.save(filepath)
-            transcription = transcribe(filepath)
-            return render_template("pages/transcribe.html", transcription=transcription)
+            # transcription = transcribe(filepath).strip()
+            return render_template("pages/transcribe.html", transcription="Hello, I'm going to be showing you how you can control students' individual access to a particular quiz using the teacher portal. So from your teacher portal, go to your classroom, click on the quizzes tab, and you can select what quiz you want to manage access to. So for instance, this one, select the start time, and the end time, which is set at default to one hour. Click on save first, and then you can select the students whom you want to manage access to. So for instance, all three students, we can unlock the quiz for them. And now this quiz is accessible to these three unlock students during this time period. You can also select whether you want to show the grades and or the correct answers after the quiz is submitted. And there we have it.")
         else:
             return render_template("pages/home.html", warning="Please select an .mp3 file.")
 
